06bc25c4c9dd094a454ba9f7ff1bf9ff36e40fd5
Westluu <west1luu@gmail.com>
2023-04-22 14:49:22 -0700

    Map search

--- cpes-hackathon/frontend/src/App.js
+++ cpes-hackathon/frontend/src/App.js
@@ -1,20 +1,6 @@
-import React, {useState} from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
-import './App.css';
+import MapWrapper from "./MapWrapper";
+import "./styles.css";
 
-function App() {
-  return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>)
+export default function App() {
+  return <MapWrapper />;
 }
-
-export default App;
