238ed6309a6525572122ab1bf4d7f879a36d26c8
chowmeinkamp <cleanslatenumberone@gmail.com>
2023-04-23 02:39:03 -0700

    "made homepage better (i think)"

--- cpes-hackathon/frontend/src/pages/home/Home.js
+++ cpes-hackathon/frontend/src/pages/home/Home.js
@@ -1,18 +1,43 @@
 import { Link } from "react-router-dom";
 import "./home.css"
+import { Icon, IconButton, SvgIcon } from "@mui/material";
 
 function Home(props) {
     return (
       <div className="Home">
-        Welcome to Cool Car Poolers!
-        <div className="Home1">
-          Connect with fellow green members, carpool, and more! Get started today!
+        <div className="HomeTop">
+          <div className="HomeTopIntro">
+            Meet carpoolers, Save the World
+          </div>
+          <div className="HomeTopDescrip">
+            Use this website to connect with other people and carpool, as well as learn how much gas ad our planet you save.
+          </div>
+          <div className="HomeTopGetStarted">
+            <Link className="link" to="/Carpool">
+              GET STARTED HERE
+            </Link>
+          </div>
         </div>
-
-        <Link className="link" to='/Carpool'><img className="clickhere" src="../../images/clickhere.png" alt="Click Here!"/></Link>
+        <div className="HomeBottom">
+          <li className="HomeBottomList">
+            <ul className="homelistitemC">
+              CONTACT US AT:
+            </ul>
+            <ul className="homelistitem">
+              (805) 439-2239 <i class="fa-solid fa-phone"></i>
+            </ul>
+            <ul className="homelistitem">
+              something@gmail.com <i className=""></i>
+            </ul>
+            <ul className="homelistitem1">
+              Florida Jail City Department 
+            </ul>
+            <ul className="homelistitem1">
+              1235, Florida, CA
+            </ul>
+          </li>
+        </div>
       </div>
-
-
     )
 }
 
