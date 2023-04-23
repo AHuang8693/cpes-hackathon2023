158872552ecc01f48a6d96042686b7e36dc6bb5b
chowmeinkamp <cleanslatenumberone@gmail.com>
2023-04-23 12:05:11 -0700

    "home contacts and about us"

--- cpes-hackathon/frontend/src/pages/info/info.jsx
+++ cpes-hackathon/frontend/src/pages/info/info.jsx
@@ -4,7 +4,23 @@ import React from "react"
 export default function Info(){
     return(
         <div className="Info">
-            hi
+            <li className="infoaboutus">
+            <ul className="titleaboutus">
+                About Us
+            </ul>
+                <ul>
+                Welcome to our carpooling platform! Our website was founded with a mission to make a positive impact on the environment by reducing the number of cars on the road. We believe that carpooling is not only a convenient and cost-effective way to travel but also a sustainable solution for our planet.
+                </ul>
+                <ul>
+                Our platform enables users to share rides with others going in the same direction. By using our website, you can connect with people in your area and save money on gas while reducing your carbon footprint. We're proud to say that we have helped our users save thousands of gallons of gas and reduce their carbon emissions.
+                </ul>
+                <ul>
+                Our team is made up of passionate individuals who are committed to creating a better world through technology. We strive to provide the best user experience possible and are constantly improving our platform to meet the needs of our users.
+                </ul>
+                <ul>
+                Thank you for choosing our website to help you find carpooling opportunities. Together, we can make a difference and create a sustainable future.
+                </ul>
+            </li>
         </div>
     )
 }
